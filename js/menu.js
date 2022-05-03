c7e2d8d4befe5e66c533815a6904c4158f34ec92
JuanMG22 <juan.m-gonzalez@outlook.com>
2022-05-03 08:50:04 -0300

    se avanzó con el responsive

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,18 @@
-document.querySelector('.menuBtn').addEventListener('click', ()=> {
-    document.querySelector('.navMenu').classList.toggle('show')
-})
+const btnMenu = document.querySelector('#menuBtn')
+const menuNav = document.querySelector('.navMenu')
+
+const link1 = document.querySelector('.link1')
+const link2 = document.querySelector('.link2')
+const link3 = document.querySelector('.link3')
+const link4 = document.querySelector('.link4')
+
+const abrirCerrarMenu = () => menuNav.classList.toggle('show')
+const cerrarMenu = () => menuNav.classList.remove('show')
+
+
+btnMenu.addEventListener('click', abrirCerrarMenu)
+
+link1.addEventListener('click', cerrarMenu)
+link2.addEventListener('click', cerrarMenu)
+link3.addEventListener('click', cerrarMenu)
+link4.addEventListener('click', cerrarMenu)
