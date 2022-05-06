8a32a778541b9c6ea4139ddc422621891e7a955f
JuanMG22 <juan.m-gonzalez@outlook.com>
2022-05-06 08:06:23 -0300

    Correccion de errores -Se hizo las vistas de laptop, tablet y mobile -Se agregó icono de cruz en menu hamburguesa -Correccion de errores

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,23 @@
 const btnMenu = document.querySelector('#menuBtn')
 const menuNav = document.querySelector('.navMenu')
+const hamburguerIcon = document.querySelector('.fa-bars')
+const timesIcon = document.querySelector('.fa-times')
 
 const link1 = document.querySelector('.link1')
 const link2 = document.querySelector('.link2')
 const link3 = document.querySelector('.link3')
 const link4 = document.querySelector('.link4')
 
-const abrirCerrarMenu = () => menuNav.classList.toggle('show')
-const cerrarMenu = () => menuNav.classList.remove('show')
+const abrirCerrarMenu = () => {
+    menuNav.classList.toggle('show')
+    hamburguerIcon.classList.toggle('d-none')
+    timesIcon.classList.toggle('d-none')
+}
+const cerrarMenu = () => {
+    menuNav.classList.remove('show')
+    hamburguerIcon.classList.toggle('d-none')
+    timesIcon.classList.toggle('d-none')
+}
 
 
 btnMenu.addEventListener('click', abrirCerrarMenu)
